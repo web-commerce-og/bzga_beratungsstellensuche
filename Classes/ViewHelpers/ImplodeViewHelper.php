2a82643770d47d3aefd8ee660960a9223b6e182f
schreibersebastian.de <breakpoint@schreibersebastian.de>
2017-07-06 14:50:11 +0200

    [TASK] Add ImplodeViewHelperTest

--- Classes/ViewHelpers/ImplodeViewHelper.php
+++ Classes/ViewHelpers/ImplodeViewHelper.php
@@ -15,6 +15,8 @@ namespace Bzga\BzgaBeratungsstellensuche\ViewHelpers;
  *
  * The TYPO3 project - inspiring people to share!
  */
+use InvalidArgumentException;
+use Traversable;
 use TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper;
 
 /**
@@ -26,6 +28,7 @@ class ImplodeViewHelper extends AbstractViewHelper
     /**
      * @param null|array|\Traversable $pieces
      * @param string $glue
+     *
      * @return string
      */
     public function render($pieces = null, $glue = ',')
@@ -34,9 +37,28 @@ class ImplodeViewHelper extends AbstractViewHelper
             $pieces = $this->renderChildren();
         }
 
-        if ($pieces instanceof \Traversable) {
-            $pieces = iterator_to_array($pieces);
+        if (! is_array($pieces) && ! $pieces instanceof Traversable) {
+            throw new InvalidArgumentException('The value is not of type array or not implementing the Traversable interface');
         }
+
+        // This is only working with objects implementing __toString method
+        if ($pieces instanceof Traversable) {
+            $pieces = iterator_to_array($pieces);
+            $this->validatePieces($pieces);
+        }
+
         return implode($glue, $pieces);
+    }
+
+    /**
+     * @param array $pieces
+     */
+    private function validatePieces(array $pieces)
+    {
+        foreach ($pieces as $piece) {
+            if (! method_exists($piece, '__toString') && ! is_scalar($piece)) {
+                throw new InvalidArgumentException('The provided value must be of type scalar or implementing the __toString method');
+            }
+        }
     }
 }
---
+++ Tests/Unit/ViewHelpers/ImplodeViewHelperTest.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace Bzga\BzgaBeratungsstellensuche\Tests\Unit\ViewHelpers;
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+use Bzga\BzgaBeratungsstellensuche\ViewHelpers\ImplodeViewHelper;
+use TYPO3\CMS\Extbase\Persistence\ObjectStorage;
+use TYPO3\CMS\Fluid\Tests\Unit\ViewHelpers\ViewHelperBaseTestcase;
+
+class ImplodeViewHelperTest extends ViewHelperBaseTestcase
+{
+
+    /**
+     * @var ImplodeViewHelper|\PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $subject;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->subject = $this->getMockBuilder(ImplodeViewHelper::class)->setMethods(['renderChildren'])->getMock();
+        $this->injectDependenciesIntoViewHelper($this->subject);
+    }
+
+    /**
+     * @test
+     * @dataProvider possibleValidValues
+     */
+    public function renderPossibleValues($input, $expected)
+    {
+        $this->subject->expects($this->once())->method('renderChildren')->willReturn($input);
+        $this->assertEquals($expected, $this->subject->render(null));
+    }
+
+    /**
+     * @test
+     * @expectedException \InvalidArgumentException
+     * @dataProvider possibleInvalidValues
+     */
+    public function renderThrowsException($pieces)
+    {
+        $this->subject->render($pieces);
+    }
+
+    /**
+     * @return array
+     */
+    public function possibleInvalidValues()
+    {
+        $objectStorage = new ObjectStorage();
+        $objectStorage->attach(new \stdClass());
+
+        return [
+            [new \stdClass()],
+            [$objectStorage]
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    public function possibleValidValues()
+    {
+        $objectStorage = new ObjectStorage();
+        $class = new ObjectToString(1);
+        $objectStorage->attach($class);
+        $class = new ObjectToString(2);
+        $objectStorage->attach($class);
+        $class = new ObjectToString(3);
+        $objectStorage->attach($class);
+
+        return [
+            [[1, 2, 3], '1,2,3'],
+            [['Title', 'Subject', 'Text'], 'Title,Subject,Text'],
+            [$objectStorage, '1,2,3']
+        ];
+    }
+}
+
+class ObjectToString
+{
+
+    /**
+     * @var string
+     */
+    private $title;
+
+    /**
+     * ObjectToString constructor.
+     *
+     * @param string $title
+     */
+    public function __construct($title)
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * @return string
+     */
+    public function __toString()
+    {
+        return (string)$this->title;
+    }
+}
