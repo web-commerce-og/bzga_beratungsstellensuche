d980ccd44293e467e7718f46b2838995fb28f798
Sebastian Schreiber <breakpoint@schreibersebastian.de>
2019-06-03 23:02:52 +0200

    [TASK] Remove old custom query building for TYPO3 version <8.4

--- Classes/ViewHelpers/Widget/Controller/PaginateController.php
+++ Classes/ViewHelpers/Widget/Controller/PaginateController.php
@@ -16,9 +16,6 @@ namespace Bzga\BzgaBeratungsstellensuche\ViewHelpers\Widget\Controller;
  * The TYPO3 project - inspiring people to share!
  */
 use Bzga\BzgaBeratungsstellensuche\Domain\Model\Dto\Demand;
-use TYPO3\CMS\Extbase\DomainObject\DomainObjectInterface;
-use TYPO3\CMS\Extbase\Persistence\Generic\LazyLoadingProxy;
-use TYPO3\CMS\Extbase\Persistence\Generic\Storage\Typo3DbQueryParser;
 use TYPO3\CMS\Extbase\Persistence\QueryInterface;
 use TYPO3\CMS\Fluid\ViewHelpers\Widget\Controller\PaginateController as CorePaginateController;
 
@@ -91,85 +88,6 @@ class PaginateController extends CorePaginateController
      */
     private function createSqlFromQuery(QueryInterface $query, Demand $demand)
     {
-        $isBelow8 = method_exists(Typo3DbQueryParser::class, 'preparseQuery');
-        $isBelow8_4 = method_exists(Typo3DbQueryParser::class, 'parseQuery');
-        $parameters = [];
-
-        $databaseConnection = $this->getDatabaseConnection();
-
-        if ($isBelow8_4) {
-            if ($isBelow8) {
-                list($hash, $parameters) = $this->queryParser->preparseQuery($query);
-            }
-            $statementParts = $this->queryParser->parseQuery($query);
-
-            $statementParts['limit']  = ((int)$query->getLimit() ?: null);
-            $statementParts['offset'] = ((int)$query->getOffset() ?: null);
-
-            $tableNameForEscape = (reset($statementParts['tables']) ?: 'foo');
-            foreach ($parameters as $parameterPlaceholder => $parameter) {
-                if ($parameter instanceof LazyLoadingProxy) {
-                    $parameter = $parameter->_loadRealInstance();
-                }
-
-                if ($parameter instanceof \DateTime) {
-                    $parameter = $parameter->format('U');
-                } elseif ($parameter instanceof DomainObjectInterface) {
-                    $parameter = (int)$parameter->getUid();
-                } elseif (is_array($parameter)) {
-                    $subParameters = [];
-                    foreach ($parameter as $subParameter) {
-                        $subParameters[] = $databaseConnection->fullQuoteStr($subParameter, $tableNameForEscape);
-                    }
-                    $parameter = implode(',', $subParameters);
-                } elseif ($parameter === null) {
-                    $parameter = 'NULL';
-                } elseif (is_bool($parameter)) {
-                    return $parameter === true ? 1 : 0;
-                } else {
-                    $parameter = $databaseConnection->fullQuoteStr((string)$parameter, $tableNameForEscape);
-                }
-
-                $statementParts['where'] = str_replace($parameterPlaceholder, $parameter, $statementParts['where']);
-            }
-
-            $statementParts = [
-                'selectFields' => implode(' ', $statementParts['keywords']) . ' ' . implode(',', $statementParts['fields']),
-                'fromTable'    => implode(' ', $statementParts['tables']) . ' ' . implode(' ', $statementParts['unions']),
-                'whereClause'  => (! empty($statementParts['where']) ? implode('', $statementParts['where']) : '1')
-                                  . (
-                                      ! empty($statementParts['additionalWhereClause'])
-                        ? ' AND ' . implode(' AND ', $statementParts['additionalWhereClause'])
-                        : ''
-                                  ),
-                'orderBy'      => ! empty($statementParts['orderings']) ? implode(
-                    ', ',
-                    $statementParts['orderings']
-                ) : '',
-                'limit'        => ($statementParts['offset'] ? $statementParts['offset'] . ', ' : '')
-                                  . ($statementParts['limit'] ? $statementParts['limit'] : ''),
-            ];
-
-            if ($demand->getLocation()) {
-                $distanceField                  = $this->geolocationService->getDistanceSqlField(
-                    $demand,
-                    $statementParts['fromTable']
-                );
-                $statementParts['selectFields'] = $distanceField . ',' . $statementParts['selectFields'];
-                $statementParts['orderBy']      = 'distance ASC';
-            }
-
-            $sql = $databaseConnection->SELECTquery(
-                $statementParts['selectFields'],
-                $statementParts['fromTable'],
-                $statementParts['whereClause'],
-                '',
-                $statementParts['orderBy'],
-                $statementParts['limit']
-            );
-
-            return $sql;
-        }
         $queryBuilder = $this->queryParser->convertQueryToDoctrineQueryBuilder($query);
         $fromParts = $queryBuilder->getQueryPart('from');
 
@@ -177,8 +95,8 @@ class PaginateController extends CorePaginateController
         $params = [];
         foreach ($queryParameters as $key => $value) {
             // prefix array keys with ':'
-                $params[':' . $key] = (is_numeric($value)) ? $value : "'" . $value . "'"; //all non numeric values have to be quoted
-                unset($params[$key]);
+            $params[':' . $key] = is_numeric($value) ? $value : "'" . $value . "'"; //all non numeric values have to be quoted
+            unset($params[$key]);
         }
 
         if ($demand->getLocation()) {
@@ -195,13 +113,5 @@ class PaginateController extends CorePaginateController
         // replace placeholders with real values
         $query = strtr($queryBuilder->getSQL(), $params);
         return $query;
-    }
-
-    /**
-     * @return \TYPO3\CMS\Core\Database\DatabaseConnection
-     */
-    private function getDatabaseConnection()
-    {
-        return $GLOBALS['TYPO3_DB'];
     }
 }
