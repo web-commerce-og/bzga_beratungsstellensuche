fe7c18bd1bd424bbf2298732c3db65b929379a76
Sebastian Schreiber <breakpoint@schreibersebastian.de>
2019-06-04 22:22:24 +0200

    [TASK] Remove custom bootstrap files for tests

--- Tests/Build/bootstrap_functional.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-// Composer autoloader for vendors
-require_once __DIR__ . '/../../Libraries/autoload.php';
-require_once __DIR__ . '/../../../../../Packages/Libraries/autoload.php';
-require_once __DIR__ . '/../../../../../typo3/sysext/core/Build/FunctionalTestsBootstrap.php';
