6c4a610aa3f3170ce0a20107b6ba8f8a6f8c5604
Sebastian Schreiber <breakpoint@schreibersebastian.de>
2019-07-08 10:13:49 +0200

    [TASK] Add StringConverter to strip all html tags from a string

---
+++ Classes/Property/TypeConverter/StringConverter.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Bzga\BzgaBeratungsstellensuche\Property\TypeConverter;
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+use Bzga\BzgaBeratungsstellensuche\Property\TypeConverterBeforeInterface;
+
+final class StringConverter implements TypeConverterBeforeInterface
+{
+    /**
+     * @var string
+     */
+    private $allowedTags = '<p><ul><li><em><i><b><br>';
+
+    /**
+     * @param mixed $source
+     * @param string $type
+     *
+     * @return bool
+     */
+    public function supports($source, $type = self::CONVERT_BEFORE): bool
+    {
+        return is_string($source) && $source !== strip_tags($source, $this->allowedTags);
+    }
+
+    /**
+     * @param mixed $source
+     * @param array $configuration
+     *
+     * @return mixed
+     */
+    public function convert($source, array $configuration = null)
+    {
+        return strip_tags($source, $this->allowedTags);
+}}
